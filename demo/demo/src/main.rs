e530adabb816c24130dcece8c7a5569d78d38dc0
calmw <calm.wang@hotmail.com>
2023-07-28 11:03:13 +0800

    Rust基础

--- demo/demo/src/main.rs
+++ demo/demo/src/main.rs
@@ -1,19 +1,8 @@
-use std::fs::File;
-use std::io;
-use std::io::{Error, Read, read_to_string};
+
 
 fn main() {
-    let s = read_from_file();
-    match s {
-        Ok(e) => println!("{}", e),
-        Ok(s) => println!("{}", s),
-        _ => {}
-    }
+    trait A{ fn foo(&self) -> Self; }
+    Box<Vec<dyn A>>
 }
 
-fn read_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("./key.txt")?; // 出错时直接抛出
-    let mut s = String::new();
-    f.read_to_string(&mut s).expect("TODO: panic message");
-    Ok(s)
-}
+
