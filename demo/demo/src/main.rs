d9e8e6bf3f7100c50f52ec263c03771d39ef5be0
calmw <calm.wang@hotmail.com>
2023-08-01 22:08:30 +0800

    Rust基础

--- demo/demo/src/main.rs
+++ demo/demo/src/main.rs
@@ -1,19 +1,3 @@
-struct IntoIter<T>(Stack<T>);
-
-// 类元组结构体
-impl<T: Clone> Iterator for IntoIter<T> {
-    type Iterm = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        if !self.0.is_empty() {
-            self.0.size -= 1;
-            self.0.data.pop()
-        } else {
-            None
-        }
-    }
-}
-
-
 struct Stack<T> {
     // 栈大小
     size: usize,
@@ -54,7 +38,7 @@ impl<T> Stack<T> {
         self.data.pop()
     }
     // 返回栈顶数据引用
-    fn peek(&self) -> Option<T> {
+    fn peek(&self) -> Option<&T> {
         if 0 == self.size { return None; }
         return self.data.get(self.size - 1);
     }
@@ -67,7 +51,63 @@ impl<T> Stack<T> {
     // into_iter,栈改变成为迭代器
     // iter,栈不变，得到不可变迭代器
     // iter_mut,栈不变，得到可变迭代器
-    fn iter(self) -> Iter<T> {}
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    fn iter(&self) -> Iter<T> {
+        let mut iterator = Iter { stack: Vec::new() };
+        for item in self.data.iter() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+
+    fn iter_mut(&mut self) -> IterMut<T> {
+        let mut iterator = IterMut { stack: Vec::new() };
+        for item in self.data.iter_mut() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+}
+
+// 类元组结构体
+struct IntoIter<T>(Stack<T>);
+
+impl<T: Clone> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.0.is_empty() {
+            self.0.size -= 1;
+            self.0.data.pop()
+        } else {
+            None
+        }
+    }
+}
+
+struct Iter<'a, T: 'a> {
+    stack: Vec<&'a T>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+
+struct IterMut<'a, T: 'a> {
+    stack: Vec<&'a mut T>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
 }
 
 fn main() {}
