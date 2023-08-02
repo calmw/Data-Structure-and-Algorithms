4523868980b075e95add927c183abe37eea4f763
calmw <calm.wang@hotmail.com>
2023-08-02 11:40:01 +0800

    Rust数据结构与算法

--- demo/demo/src/main.rs
+++ demo/demo/src/main.rs
@@ -1,13 +1,13 @@
 #[derive(Debug)]
-struct Queue<T> {
+struct Deque<T> {
     // 容量
     cap: usize,
     // 数据容器
     data: Vec<T>,
 }
 
-impl<T> Queue<T> {
-    // 初始化队列
+impl<T> Deque<T> {
+    // 初始化双端队列
     fn new() -> Self {
         Self {
             cap: 0,
@@ -17,9 +17,11 @@ impl<T> Queue<T> {
     fn is_empty(&self) -> bool {
         0 == Self::len(&self)
     }
+
     fn len(&self) -> usize {
         self.data.len()
     }
+
     fn is_full(&self) -> bool {
         self.len() == self.cap
     }
@@ -30,7 +32,14 @@ impl<T> Queue<T> {
     }
 
     // 向队列中插入数据
-    fn enqueue(&mut self, val: T) -> Result<(), String> {
+    fn add_front(&mut self, val: T) -> Result<(), String> {
+        if self.len() >= self.cap {
+            return Err("No space available".to_string());
+        }
+        self.data.push(val);
+        Ok(())
+    }
+    fn add_rear(&mut self, val: T) -> Result<(), String> {
         if self.len() >= self.cap {
             return Err("No space available".to_string());
         }
@@ -38,11 +47,18 @@ impl<T> Queue<T> {
         Ok(())
     }
     // 数据出队
-    fn dequeue(&mut self) -> Option<T> {
+    fn remove_front(&mut self) -> Option<T> {
         if self.len() == 0 {
             None
         } else {
             self.data.pop()
+        }
+    }
+    fn remove_rear(&mut self) -> Option<T> {
+        if self.len() == 0 {
+            None
+        } else {
+            Some(self.data.remove(0))
         }
     }
 
@@ -55,24 +71,24 @@ impl<T> Queue<T> {
     }
 
     fn iter(&self) -> Iter<T> {
-        let mut iterator = Iter { queue: Vec::new() };
+        let mut iterator = Iter { deque: Vec::new() };
         for item in self.data.iter() {
-            iterator.queue.push(item);
+            iterator.deque.push(item);
         }
         iterator
     }
 
     fn iter_mut(&mut self) -> IterMut<T> {
-        let mut iterator = IterMut { queue: Vec::new() };
+        let mut iterator = IterMut { deque: Vec::new() };
         for item in self.data.iter_mut() {
-            iterator.queue.push(item);
+            iterator.deque.push(item);
         }
         iterator
     }
 }
 
 // 类元组结构体
-struct IntoIter<T>(Queue<T>);
+struct IntoIter<T>(Deque<T>);
 
 impl<T: Clone> Iterator for IntoIter<T> {
     type Item = T;
@@ -87,24 +103,24 @@ impl<T: Clone> Iterator for IntoIter<T> {
 }
 
 struct Iter<'a, T: 'a> {
-    queue: Vec<&'a T>,
+    deque: Vec<&'a T>,
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.queue.remove(0))
+        Some(self.deque.remove(0))
     }
 }
 
 struct IterMut<'a, T: 'a> {
-    queue: Vec<&'a mut T>,
+    deque: Vec<&'a mut T>,
 }
 
 impl<'a, T> Iterator for IterMut<'a, T> {
     type Item = &'a mut T;
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.queue.remove(0))
+        Some(self.deque.remove(0))
     }
 }
 
