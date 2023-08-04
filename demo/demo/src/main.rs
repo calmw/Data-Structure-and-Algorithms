4ce9ff10e3ed83841c963620cdb9bdc2f9f39e77
calmw <calm.wang@hotmail.com>
2023-08-04 22:41:18 +0800

    Rust数据结构与算法

--- demo/demo/src/main.rs
+++ demo/demo/src/main.rs
@@ -2,6 +2,8 @@ fn sequential_search_pos(nums: &[i32], num: i32) -> Option<usize> {
     let mut pos: usize = 0;
     let mut found = false;
     while pos < nums.len() {
+        // 这里默认结合中的数据无序，如果是有序的，可增加集合中索引为0的值与num进行大小比对，
+        // 如果不满足，就可以直接终止
         if num == nums[pos] {
             found = true;
             return Some(pos)
