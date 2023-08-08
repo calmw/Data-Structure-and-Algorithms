69564ed8b5a7853ce5464fa1f4affa9392ee07c6
calmw <calm.wang@hotmail.com>
2023-08-08 10:02:32 +0800

    Rust数据结构与算法

--- demo/demo/src/main.rs
+++ demo/demo/src/main.rs
@@ -1,32 +1,20 @@
-fn quick_sort2(nums: &mut [i32], low: usize, high: usize) {
-    if low > high { return; }
-    let mut lm = low;
-    let mut rm = high;
-    while lm < rm {
-        // 将右标记不断左移
-        while lm < rm && nums[low] <= nums[rm] {
-            rm -= 1;
+fn insertion_sort(nums: &mut [i32]) {
+    if nums.len() < 2 { return; }
+    for i in 1..nums.len() {
+        let mut pos = i;
+        let curr = nums[i];
+        while pos > 0 && curr < nums[pos - 1] {
+            nums[pos] = nums[pos - 1];// 向右移动数据
+            pos -= 1;
         }
-        // 将左标记不断右移
-        while lm < rm && nums[lm] <= nums[low] {
-            lm += 1;
-        }
-        // 交换左右标记处的值
-        nums.swap(lm, rm)
+        nums[pos] = curr; // 插入数据
     }
-    // 交换分割点数据
-    nums.swap(low, lm);
-    if lm > 1 {
-        quick_sort2(nums, low, lm - 1);
-    }
-    quick_sort2(nums, rm + 1, high);
 }
-
 
 fn main() {
     let mut nums = [12, 78, 789, 22, 56, 86, 2, 45, 213, 789, 124, 6880, 35];
     let high = nums.len() - 1;
-    quick_sort2(&mut nums, 0, high);
+    insertion_sort(&mut nums);
     println!("sorted nums: {:?}", nums)
 }
 
